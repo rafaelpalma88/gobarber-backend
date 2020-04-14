58613ed2d6c201da392390a4d6a3560315be4dc7
Rafael Costa Palma <palma3482@gmail.com>
2020-04-14 13:53:34 -0300

    first commmit

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,40 @@
+import Appointment from '../models/Appointment'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+
+interface CreateAppointmentDTO {
+    provider: string,
+    date: Date
+}
+
+class AppointmentRepository {
+    private appointments: Appointment[]
+
+    constructor() {
+        this.appointments = []
+    }
+
+    public all(): Appointment[] {
+
+        return this.appointments;
+
+    }
+
+    public create({provider, date}: CreateAppointmentDTO): Appointment {
+        const appointment = new Appointment({ provider, date });
+
+        this.appointments.push(appointment);
+
+        return appointment;
+    }
+
+    public findByDate(date: Date): Appointment | null {
+
+        const findAppointment = this.appointments.find(appointment =>
+            isEqual(date, appointment.date)
+        );
+
+        return findAppointment || null;
+    }
+}
+
+export default AppointmentRepository;
