ed819b4d98456e514a04f209c28f101a5b634dab
Rafael Costa Palma <rafael.palma@opah.com.br>
2020-05-15 17:16:27 -0300

    retrieving availability

---
+++ src/modules/appointments/services/ListProvidersMonthAvailabiltyService.ts
@@ -0,0 +1,61 @@
+import { injectable, inject } from 'tsyringe'
+//import User from '@modules/users/infra/typeorm/entities/User'
+import { getDaysInMonth, getDate } from 'date-fns'
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
+
+interface IRequest {
+    provider_id: string;
+    month: number;
+    year: number;
+}
+
+type IResponse = Array<{
+    day: number;
+    available: boolean;
+}>
+
+@injectable()
+class ListProviderMonthAvailability {
+
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository
+    ) {}
+
+    public async execute({provider_id, year, month}: IRequest): Promise<IResponse> {
+        const appointments = await this.appointmentsRepository.findAllInMonthFromProvider({
+            provider_id,
+            year,
+            month
+        })
+
+        console.log(appointments)
+        const numberOfDaysInMonth = getDaysInMonth(
+            new Date(year, month - 1)
+        )
+
+        const eachDayArray = Array.from(
+            { length: numberOfDaysInMonth },
+            ( value, index ) => index + 1
+        )
+
+        console.log(eachDayArray)
+        const availability = eachDayArray.map(day => {
+            const appointmentsInDay = appointments.filter(appointment => {
+                return getDate(appointment.date) === day;
+            })
+
+            return {
+                day,
+                available: appointmentsInDay.length < 10
+            }
+        })
+
+        return availability
+
+    }
+
+}
+
+export default ListProviderMonthAvailability;
